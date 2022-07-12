4453babad69e3ad0a772e7e4a5ae4910164881ce
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-07-12 12:28:37 +0530

    classes

--- src/Routes/Advanced/Advance.routes.jsx
+++ src/Routes/Advanced/Advance.routes.jsx
@@ -1,6 +1,24 @@
 import { Fragment } from "react";
 
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greetings() {
+    console.log(`hello, my name is ${this.name}`);
+  }
+}
+
 const Advance = () => {
+  const userData = {
+    name: "Jake",
+    age: 45,
+  };
+  console.log(userData);
+  const userObj = new User("Jame", 34);
+  console.log(userObj);
+  userObj.greetings();
   return (
     <Fragment>
       <div>THis is Advance</div>
